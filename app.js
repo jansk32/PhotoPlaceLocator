97ee0efd12d2b61c6f7b8a24f483f7c66c0114fd
Jansen Kwong <j.kwong4@student.unimelb.edu.au>
2019-06-13 06:21:56 +1000

    create boiler plate for personal project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.set('views', './views');
+app.set('view engine','pug');
+
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req,res){
+    res.render('map');
+})
+
+app.listen(port);
+console.log("Listening to port " + port);
